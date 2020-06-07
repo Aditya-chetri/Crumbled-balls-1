6d1f6a6afb6b7a6b4381289b02201d073196676e
Aditya-chetri <64538450+Aditya-chetri@users.noreply.github.com>
2020-06-07 22:09:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+    engine = Engine.create();
+	world = engine.world;
+	Engine.run(engine);
+  platform = new Ground(400, 680, 800, 40);
+  paper = new Ball(100, 675, 20, 20);
+  part1 = new DustbinPart(620, 655, 140, 10);
+  part2 = new DustbinPart(550, 620, 10, 80);
+  part3 = new DustbinPart(690, 620, 10, 80);
+  
+}
+function draw() {
+  background(0);
+  platform.display();
+  paper.display();
+  part1.display();
+  part2.display();
+  part3.display();
+  
+  
+  drawSprites();
+  
+}
+
+function keyPressed(){ 
+  if(keyCode === UP_ARROW){ 
+    Matter.Body.applyForce(paper.body, paper.body.position, {x: 60, y: -60}); 
+  } 
+}
+
+
+
